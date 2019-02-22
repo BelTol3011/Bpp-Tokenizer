716bc30e7961bdf290fc8f83bc7e4a1ece9cad0c
BELTOL3011 <bela.tollking@gmail.com>
2019-02-22 21:59:34 +0100

    Updated

--- Main.py
+++ Main.py
@@ -1,4 +1,26 @@
 import sys
+import Parser
+import Tokenize
+
+# Kriegt Dateinamen un outputnamen als Argumente.
+# ZU kompiliertem File
+#  - Parsen
+#  - Tokenize
+#  - Kompilieren
 
 arguments = sys.argv
 
+print(arguments)
+
+
+if arguments[0] == "python" or arguments[0] == "python3": del arguments[0]
+
+filename = arguments[1]
+outputfilename = arguments[2]
+
+print("Converting", filename, "to", outputfilename + ".")
+print("Opening File...")
+infile = open(filename)
+inlist = infile.read().split("\n")
+print("Parsing...")
+parsed = Parser.Pars(inlist)

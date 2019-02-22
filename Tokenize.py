9de21f916d84bc3ad1856ea01c9d634afd3e476f
Jhondah <joehondah@googlemail.com>
2019-02-22 16:18:29 +0100

    update bla

--- Tokenize.py
+++ Tokenize.py
@@ -1,4 +1,28 @@
 import sys
+import dump
+
 arguments = sys.argv
 
+# Strg+Alt+L : Reformat
 
+# Aufgaben:
+# bekommt eine 2D-Liste, geschachtelt mit Tupels
+# ({Argument1}{Obj1})
+# ({Argument2}{Obj2})
+#
+# - File in tokens umwandeln
+# - In eine dump datei speichern
+#
+#
+testliste = [
+    ['a']['Variable'],
+    ['=']['Operator'],
+    ['1'][''],
+]
+
+
+def tokenize(inputliste):
+    for line in inputliste:
+        print()
+
+    # endfor

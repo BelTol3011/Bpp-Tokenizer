f8567b1155fd4f1f992a0133b67da66acb40354b
bela <bela.tollking@gmail.com>
2021-06-04 17:48:37 +0200

    lelele

--- deployment_engine.py
+++ deployment_engine.py
@@ -1,12 +1,17 @@
-from typing import Literal
-
 Code = list[str]
-Path = list[str]
 
 
-def get_mcfunction_str(namespace: str, path: Path):
-    path_str = "/".join(path)
-    return f"{namespace}:{path_str}"
+# Path = tuple[str, list[str]]
+
+
+class Path(tuple[str, list[str]]):
+    def __hash__(self):
+        return hash(get_mcfunction_str(self))
+
+
+def get_mcfunction_str(path: Path):
+    path_str = "/".join(path[1])
+    return f"{path[0]}:{path_str}"
 
 
 def split_list_file(input_code: Code) -> dict[str: Code]:
@@ -53,7 +58,7 @@ def karma_util(string: str, search_char: str = "$") -> list[int]:
     return indeces
 
 
-def link(code: Code, function_dict: dict[str: Path]) -> Code:
+def link(code: Code, function_dict: dict[str: tuple[Path, Code]]) -> Code:
     """replaces the function tag $function with the real function path namespace:.../function
     Example ( namespace="namespace", function_dict={"mcutils": ["mcutils", "create_object"]} ):
         $foo -> namespace:path/.../foo
@@ -64,44 +69,70 @@ def link(code: Code, function_dict: dict[str: Path]) -> Code:
     code = "\n".join(code)
 
     indeces = karma_util(code, "$")  # indeces of the $ symbols in the file string
-    print(f"{indeces=}")
+    # print(f"{indeces=}")
+    for index in indeces:
+        name = ""
+        i = 1
+        while (char := code[index + i]) not in [" ", "\n"]:
+            name += char
+            i += 1
+
+        if code[index+1] != "!":
+            replacement = get_mcfunction_str(function_dict[name][0])
+        else:
+            replacement = "\n".join(function_dict[name][1])
+        # print(mcfunction_string)
+        code = code[:index] + replacement + code[index + len(replacement):]
+
+    return code.split("\n")
 
 
-def _to_datapack(functions: list[Code], libmcutils: Code, libmcutils_prefix: Path):
+def batch_link(function_lists: list[tuple[Path, Code]]):
     """
-    :param functions: The .list.mcfunction file of all functions
-    :param libmcutils: The code for the libmcutils library
-    :param libmcutils_prefix: The path to libmcutils in the datapack
+    :param function_lists: A list of Paths and .list.mcfunction files
     :return:
     """
+    functions_namescodes: dict[str: tuple[Path, Code]] = {}
     print("Splitting...")
+    for path, function_list in function_lists:
+        print(f"\t{get_mcfunction_str(path)}")
+        _functions = split_list_file(function_list)
 
-    files = split_list_file(libmcutils)
+        for name in _functions:
+            print(f"\t\t{name}")
+            # functions_code.update({name: _functions[name]})
+            # extend the path to contain the function name
+            _path = Path((path[0], path[1] + [name]))
+            functions_namescodes.update({name: (_path, _functions[name])})
 
-    files = split_list_file(libmcutils)
-
-    print("Finished!")
-    print("Linking libmcutils...")
-
-    # generate function name dict
-    function_dict: dict[str: Path] = {}
-    for file in files:
-        function_dict.update({file: libmcutils_prefix + [file]})
+    print("Linking...")
 
     # link
-    linked_functions: dict[str: Code] = {}
-    for function in functions:
-        print(f"linking {function}...", end="")
+    linked_functions: dict[Path: Code] = {}
+    for name in functions_namescodes:
+        path, code = functions_namescodes[name]
+        print(f"\t{get_mcfunction_str(path)}")
 
-        linked_code = link(functions[function], function_dict)
+        linked_code = link(code, functions_namescodes)
 
-        linked_functions.update({f})
+        linked_functions.update({path: linked_code})
+
+    print("Listing:")
+    for path in linked_functions:
+        print(f"==> {get_mcfunction_str(path)}")
+        print("\n".join(linked_functions[path]))
 
 
-if __name__ == "__main__":
+def main():
     """How to compile:
     1. split, link and copy libmcutils into the 'mcutils' path
     2. generate the unlinked user code (.list.mcfunction) via the python script
     3. link and copy the user code with the mcutils function locations
     """
-    split_list_file()
+    with open("mcutils/mcutils.list.mcfunction", "r") as f:
+        code = f.read().split("\n")
+    batch_link([(Path(("mcutils", [])), code)])
+
+
+if __name__ == "__main__":
+    main()
